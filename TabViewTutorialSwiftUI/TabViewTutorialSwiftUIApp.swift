d2b9ed8ded1a6a21c9b942d91e8e4ed0fd1158af
osvaldsoza <osvald.soza@gmail.com>
2024-04-05 20:29:40 -0300

    Initial Commit

---
+++ TabViewTutorialSwiftUI/TabViewTutorialSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewTutorialSwiftUIApp.swift
+//  TabViewTutorialSwiftUI
+//
+//  Created by Osvaldo Souza Ferreira on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewTutorialSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
